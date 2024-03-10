5c1fcf165d1a88e88109a7c0dfd071564b90bcae
bayzman <bassmaninno102@gmail.com>
2024-03-09 18:42:12 -0800

    console

--- console.py
+++ console.py
@@ -189,7 +189,7 @@ class HBNBCommand(cmd.Cmd):
             except Exception as e:
                 print(e)
 
-        elif (args_len > 4:
+        if (args_len > 4:
             return
 
             obj_name, obj_id, attr_name, value = args.split()
