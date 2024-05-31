6c03fdfdbd839055f31fd5b28d2ffa398ffb7f5e
Bayzman <bassmaninno102@gmail.com>
2024-05-31 02:58:13 -0700

    fixed base_model

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            models.storage.new()
         else:
             self.__dict__.update(kwargs)
 
