a28b4a3b5d8b1077b52dbbcd05baa897149c9591
Bayzman <bassmaninno102@gmail.com>
2024-06-01 14:11:41 -0700

    fixed file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -36,5 +36,5 @@ class FileStorage:
                 obj_json = json.load(f)
                 for obj in obj_json.values():
                     obj_class = obj["__class__"]
-                    obj = obj_class(**obj)
-                    self.new(obj)
+                    del obj['__class__']
+                    self.new(eval(obj_class)(**obj))
